9e1a2c565f3a9dc39aadfe3bb2de750aca5478ca
spences10 <spences10apps@gmail.com>
2021-11-23 18:26:21 +0000

    feat: :sparkles: add dots component

---
+++ components/dots.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :style="btnStyles"/>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "#10183e"
+    },
+    bgColor: {
+      type: String,
+      default: "#121212"
+    },
+  },
+  computed: {
+    btnStyles() {
+      return {
+        position: "absolute",
+        right: "0",
+        top: "0",
+        width: "6.7vw",
+        height: "100vh",
+        "background-image": `radial-gradient(${this.color} 20%, ${this.bgColor} 20%)`,
+        "background-size": "25px 25px",
+      };
+    }
+  }
+};
+</script>
