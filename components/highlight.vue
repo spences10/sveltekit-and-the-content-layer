47a2c0c9dece5cc46061df7a6516a725510fe987
spences10 <spences10apps@gmail.com>
2021-11-23 19:16:16 +0000

    feat: :sparkles: add highlight component

---
+++ components/highlight.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :style="btnStyles"><slot></slot></span>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "#f5487fff"
+    },
+  },
+  computed: {
+    btnStyles() {
+      return {
+        color: this.color,
+      };
+    }
+  }
+};
+</script>
