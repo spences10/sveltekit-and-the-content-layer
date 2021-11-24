1d1c0285043857eab9a19991e639fd10420ff3c2
spences10 <spences10apps@gmail.com>
2021-11-24 12:29:28 +0000

    refactor: :recycle: add more variables
    for adjusting the background size

--- components/dots.vue
+++ components/dots.vue
@@ -1,31 +1,48 @@
 <template>
-  <div :style="btnStyles"/>
+  <div :style="styles"/>
 </template>
 
 <script>
-export default {
-  props: {
-    color: {
-      type: String,
-      default: "#10183e"
+  export default {
+    props: {
+      width: {
+        type: String,
+        default: "6.7vw"
+      },
+      rgColor: {
+        type: String,
+        default: "#10183e"
+      },
+      rgBgColor: {
+        type: String,
+        default: "#121212"
+      },
+      rgColorSize: {
+        type: String,
+        default: "18%"
+      },
+      rgBgColorSize: {
+        type: String,
+        default: "20%"
+      },
+      bgSize: {
+        type: String,
+        default: "25px"
+      },
     },
-    bgColor: {
-      type: String,
-      default: "#121212"
-    },
-  },
-  computed: {
-    btnStyles() {
-      return {
-        position: "absolute",
-        right: "0",
-        top: "0",
-        width: "6.7vw",
-        height: "100vh",
-        "background-image": `radial-gradient(${this.color} 20%, ${this.bgColor} 20%)`,
-        "background-size": "25px 25px",
-      };
+    
+    computed: {
+      styles() {
+        return {
+          position: "absolute",
+          right: "0",
+          top: "0",
+          width: `${this.width}`,
+          height: "100vh",
+          "background-image": `radial-gradient(${this.rgColor} ${this.rgColorSize}, ${this.rgBgColor} ${this.rgBgColorSize})`,
+          "background-size": `${this.bgSize} ${this.bgSize}`,
+        };
+      }
     }
-  }
-};
+  };
 </script>
