5d7d2972b80e9d6fb25a8130efd29365854a6b01
spences10 <spences10apps@gmail.com>
2021-12-05 15:14:26 +0000

    feat: :sparkles: add GraphQL client logos

---
+++ components/graphql-clients.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-clicks>
+    <img class="houdini" src="/assets/houdini.png" alt="houdini logo" />
+    <img
+      class="urql"
+      src="/assets/urql-logo.svg"
+      alt="urql logo"
+    />
+  </v-clicks>
+</template>
+
+<style scoped>
+.houdini {
+  position: absolute;
+  left: 130px;
+  top: 100px;
+  width: 40%;
+}
+.urql {
+  position: absolute;
+  left: 510px;
+  top: 260px;
+  width: 20%;
+}
+</style>
