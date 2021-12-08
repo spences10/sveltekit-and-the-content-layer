677d3de3663c0bfa2472442641570e992e9ff8c8
spences10 <spences10apps@gmail.com>
2021-12-08 20:56:10 +0000

    feat: :sparkles: add but component

---
+++ components/butt.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-motion :initial="{ x: -80 }" :enter="{ x: 10 }">
+    <img
+      class="butt"
+      src="https://res.cloudinary.com/defkmsrpw/image/upload/q_auto,f_auto/v1614936696/scottspence.com/buttbutt.png"
+      alt="butt butt"
+    />
+  </div>
+</template>
+
+<style scoped>
+.butt {
+  position: absolute;
+  left: 150px;
+  top: -240px;
+  width: 70%;
+}
+</style>
